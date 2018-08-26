f4c7aa0e993f85d3e33abaaecd8318c128aa5698
kumokay <kumokay@gmail.com>
2018-08-25 21:20:25 -0700

    add src / dst into network topo

--- placethings/topology.py
+++ placethings/topology.py
@@ -17,31 +17,29 @@ log = logging.getLogger()
 
 class TopoGraph(object):
 
-    _DEFAULT_N_SWITCH = 10
-    _DEFAULT_N_DEVICE = 5
     _DEFAULT_DENSITY = 0.3
     _DEFAULT_LATENCY = Unit.ms(20)
     _DEFAULT_BANDWIDTH = Unit.mbyte(300)
-    _DEFAULT_SWITCH_PREFIX = 'sw'
-    _DEFAULT_DEVICE_PREFIX = 'dv'
+    # default nodes in the topology if not present in input arguments
+    _DEFAULT_N_SWITCH = 10
+    _DEFAULT_N_DEVICE = 5
+    _DEFAULT_N_SOURCE = 3
+    _DEFAULT_N_DESTINATION = 1
+    _DEFAULT_PREFIX_SWITCH = 'sw'
+    _DEFAULT_PREFIX_DEVICE = 'dv'
+    _DEFAULT_PREFIX_SOURCE = 'src'
+    _DEFAULT_PREFIX_DESTINATION = 'dst'
 
     @classmethod
     def create_default_switch_list(cls, n_switch=None):
+        # expose this function to network graph
         if not n_switch:
             n_switch = cls._DEFAULT_N_SWITCH
-        switch_list = [
-            '{}.{}'.format(cls._DEFAULT_SWITCH_PREFIX, i)
-            for i in range(n_switch)]
-        return switch_list
+        return cls._create_node_list(n_switch, cls._DEFAULT_PREFIX_SWITCH)
 
     @classmethod
-    def _create_default_device_list(cls, n_device=None):
-        if not n_device:
-            n_device = cls._DEFAULT_N_DEVICE
-        device_list = [
-            '{}.{}'.format(cls._DEFAULT_DEVICE_PREFIX, i)
-            for i in range(n_device)]
-        return device_list
+    def _create_node_list(cls, n_node, node_prefix):
+        return ['{}.{}'.format(node_prefix, i) for i in range(n_node)]
 
     @classmethod
     def _add_random_link(cls, graph, src, dst):
@@ -52,7 +50,12 @@ class TopoGraph(object):
         graph.add_edge(src, dst, **attr)
 
     @classmethod
-    def create_default_topo(cls, switch_list=None, device_list=None):
+    def create_default_topo(
+            cls,
+            switch_list=None,
+            device_list=None,
+            source_list=None,
+            dst_list=None):
         """
         Randomly deploy devices in a newtork consists of randomly connected
         switches.
@@ -60,13 +63,24 @@ class TopoGraph(object):
         Args:
             switch_list (list): a list of APs, routers, switchs, etc
             device_list (list): a list of devices
+            source_list (list): a list of data sources
+            dst_list (list): a list of actuators
         Returns:
             topology (networkx.DiGraph)
         """
         if not switch_list:
-            switch_list = cls.create_default_switch_list()
+            switch_list = cls._create_node_list(
+                cls._DEFAULT_N_SWITCH, cls._DEFAULT_PREFIX_SWITCH)
         if not device_list:
-            device_list = cls._create_default_device_list()
+            device_list = cls._create_node_list(
+                cls._DEFAULT_N_DEVICE, cls._DEFAULT_PREFIX_DEVICE)
+        if not source_list:
+            source_list = cls._create_node_list(
+                cls._DEFAULT_N_SOURCE, cls._DEFAULT_PREFIX_SOURCE)
+        if not dst_list:
+            dst_list = cls._create_node_list(
+                cls._DEFAULT_N_DESTINATION, cls._DEFAULT_PREFIX_DESTINATION)
+
         graph = nx.DiGraph()
         graph.add_nodes_from(switch_list)
         n_edge_per_switch = int(round(cls._DEFAULT_DENSITY * len(switch_list)))
@@ -76,11 +90,12 @@ class TopoGraph(object):
                     continue
                 cls._add_random_link(graph, s1, s2)
                 cls._add_random_link(graph, s2, s1)
-        graph.add_nodes_from(device_list)
-        for device in device_list:
+        client_list = device_list + source_list +dst_list
+        graph.add_nodes_from(client_list)
+        for node in client_list:
             # connect device to a random selected switch
             [switch] = random.sample(switch_list, 1)
             # randomly assign bandwidth and latency
-            cls._add_random_link(graph, device, switch)
-            cls._add_random_link(graph, switch, device)
+            cls._add_random_link(graph, node, switch)
+            cls._add_random_link(graph, switch, node)
         return graph
