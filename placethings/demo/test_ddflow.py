7bea643c06648d2bcd98110085662a01e94bf35f
kumokay <kumokay@gmail.com>
2018-10-06 13:09:28 -0700

    update scenario

--- placethings/demo/test_ddflow.py
+++ placethings/demo/test_ddflow.py
@@ -31,43 +31,34 @@ class TestPhase1(BaseTestCase):
         topo, topo_device_graph, Gd, G_map = cfgHelper.get_graphs()
         if is_simulate:
             cls._simulate(topo_device_graph, Gd, G_map)
-        all_devices = [
-            'P3_2XLARGE.0',
-            'T3_LARGE.0',
-            'T2_MICRO.0']
-        all_sws = [
-            'CENTER_SWITCH.0',
-            'FIELD_SWITCH.0',
-            'FIELD_SWITCH.1',
-            'BB_SWITCH.0',
-            'BB_SWITCH.1',
-            'BB_SWITCH.2']
+        # all_devices = [
+        #     'P3_2XLARGE.0',
+        #     'T3_LARGE.0',
+        #     'T2_MICRO.0']
+        # all_sws = [
+        #     'CENTER_SWITCH.0',
+        #     'FIELD_SWITCH.0',
+        #     'FIELD_SWITCH.1',
+        #     'BB_SWITCH.0',
+        #     'BB_SWITCH.1',
+        #     'BB_SWITCH.2']
         update_id = -1
-        for i in range(1, 50):
+        for i in range(1, 100):
             update_id += 1
             log.info('=== update {}: dev-nw_dev link ==='.format(update_id))
-            edges = list(topo_device_graph.edges())
-            [dev] = random.sample(all_devices, 1)
-            [nw_dev] = random.sample(all_sws, 1)
-            while (dev, nw_dev) not in edges:
-                [dev] = random.sample(all_devices, 1)
-                [nw_dev] = random.sample(all_sws, 1)
-            latency = Unit.ms(random.randint(5, 3000))
+            dev = 'P3_2XLARGE.0'
+            nw_dev = 'CENTER_SWITCH.0'
+            latency = Unit.ms(random.randint(2, 5000))
             cfgHelper.update_dev_link_latency(dev, nw_dev, latency)
-            cfgHelper.update_topo_device_graph()
-            if is_update_map:
-                cfgHelper.update_task_map()
-            cfgHelper.update_max_latency_log()
-            topo, topo_device_graph, Gd, G_map = cfgHelper.get_graphs()
-            if is_simulate:
-                cls._simulate(topo_device_graph, Gd, G_map)
-            log.info('=== update {}: nw_dev-nw_dev link ==='.format(update_id))
-            edges = list(topo_device_graph.edges())
-            [nw_dev1, nw_dev2] = random.sample(all_sws, 2)
-            while (nw_dev1, nw_dev2) not in edges:
-                [nw_dev1, nw_dev2] = random.sample(all_sws, 2)
-            latency = Unit.ms(random.randint(5, 200))
-            cfgHelper.update_nw_link_latency(nw_dev1, nw_dev2, latency)
+            dev = 'CONTROLLER.0'
+            nw_dev = 'CENTER_SWITCH.0'
+            latency = Unit.ms(random.randint(2, 500))
+            cfgHelper.update_dev_link_latency(dev, nw_dev, latency)
+            dev = 'T3_LARGE.0'
+            nw_dev = 'FIELD_SWITCH.1'
+            latency = Unit.ms(random.randint(2, 5000))
+            cfgHelper.update_dev_link_latency(dev, nw_dev, latency)
+            # update graph and map
             cfgHelper.update_topo_device_graph()
             if is_update_map:
                 cfgHelper.update_task_map()
@@ -106,10 +97,6 @@ class TestPhase2(BaseTestCase):
         cfgHelper2.update_max_latency_log()
         all_mobiles = [
             'CAMERA.0']
-        all_sws = [
-            'BB_SWITCH.0',
-            'BB_SWITCH.1',
-            'BB_SWITCH.2']
         all_aps = [
             'BB_AP.0',
             'BB_AP.1',
@@ -125,27 +112,34 @@ class TestPhase2(BaseTestCase):
             nw_dev = edges[0][1]
             assert nw_dev in all_aps, '{} not in {}'.format(nw_dev, all_aps)
             new_nw_dev = random.sample(all_aps, 1)[0]
-            new_latency = Unit.ms(random.randint(1, 20))
+            new_latency = Unit.ms(random.randint(30, 50))
             cfgHelper.update_dev_link(dev, nw_dev, new_nw_dev, new_latency)
             cfgHelper2.update_dev_link(dev, nw_dev, new_nw_dev, new_latency)
-            log.info('=== update {}: nw_dev-nw_dev link ==='.format(update_id))
-            _, topo_device_graph, _, _ = cfgHelper.get_graphs()
-            edges = list(topo_device_graph.edges())
-            [nw_dev1, nw_dev2] = random.sample(all_sws + all_aps, 2)
-            while (nw_dev1, nw_dev2) not in edges:
-                [nw_dev1, nw_dev2] = random.sample(all_sws + all_aps, 2)
-            latency = Unit.ms(random.randint(5, 200))
+            log.info('=== update {}: ap-bb link ==='.format(update_id))
+            ap_bb_links = [
+                ('BB_AP.0', 'BB_SWITCH.0'),
+                ('BB_AP.1', 'BB_SWITCH.1'),
+                ('BB_AP.2', 'BB_SWITCH.2')]
+            [which_link] = random.sample(ap_bb_links, 1)
+            which_rand = random.randint(0, 9)
+            if which_rand < 2:
+                latency = Unit.ms(random.randint(5000, 50000))
+            else:
+                latency = Unit.ms(random.randint(10, 40))
+            nw_dev1, nw_dev2 = which_link
             cfgHelper.update_nw_link_latency(nw_dev1, nw_dev2, latency)
             cfgHelper2.update_nw_link_latency(nw_dev1, nw_dev2, latency)
             log.info('=== update {}: ap-ap link ==='.format(update_id))
-            _, topo_device_graph, _, _ = cfgHelper2.get_graphs()
-            edges = list(topo_device_graph.edges())
-            [nw_dev1, nw_dev2] = random.sample(all_aps, 2)
-            while (nw_dev1, nw_dev2) not in edges:
-                [nw_dev1, nw_dev2] = random.sample(all_aps, 2)
-            latency = Unit.ms(random.randint(25, 100))
+            # _, topo_device_graph, _, _ = cfgHelper2.get_graphs()
+            # edges = list(topo_device_graph.edges())
+            ap_ap_links = [
+                ('BB_AP.0', 'BB_AP.1'),
+                ('BB_AP.1', 'BB_AP.2')]
+            [which_link] = random.sample(ap_ap_links, 1)
+            latency = Unit.ms(random.randint(30, 50))
+            nw_dev1, nw_dev2 = which_link
             cfgHelper2.update_nw_link_latency(nw_dev1, nw_dev2, latency)
-            # update graph and log
+            log.info('=== update {}: update graphs ==='.format(update_id))
             cfgHelper.update_topo_device_graph()
             cfgHelper2.update_topo_device_graph()
             if is_update_map:
