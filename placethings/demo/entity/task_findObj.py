3a450b5b85e5da9d2b977438b2ec618d76c323b2
kumokay <kumokay@gmail.com>
2018-10-30 20:50:43 -0700

    tttt

--- placethings/demo/entity/task_findObj.py
+++ placethings/demo/entity/task_findObj.py
@@ -32,21 +32,32 @@ class RPCServer(BaseTask.RPCServer):
         t1 = time.time()
         log.info('(TIME) start computation: {}'.format(t1))
         log.info('finding object')
-        yolo_folder = '/opt/github/darknet'
-        image_data = base64.b64decode(data)
-        image = Image.open(io.BytesIO(image_data))
-        img_name = 'img_{}.png'.format(self._imgid)
-        self._imgid += 1
-        image.save('{}/{}'.format(yolo_folder, img_name), 'PNG')
-        cmd = (
-            './darknet detect cfg/yolov3-tiny.cfg '
-            'yolov3-tiny.weights {}').format(img_name),
-        proc = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, shell=True, cwd=yolo_folder,)
-        result = proc.communicate()[0]
+        if False:
+            yolo_folder = '/opt/github/darknet'
+            image_data = base64.b64decode(data)
+            image = Image.open(io.BytesIO(image_data))
+            img_name = 'img_{}.png'.format(self._imgid)
+            self._imgid += 1
+            image.save('{}/{}'.format(yolo_folder, img_name), 'PNG')
+            cmd = (
+                './darknet detect cfg/yolov3-tiny.cfg '
+                'yolov3-tiny.weights {}').format(img_name),
+            proc = subprocess.Popen(
+                cmd, stdout=subprocess.PIPE, shell=True, cwd=yolo_folder,)
+            result = proc.communicate()[0]
+            if self.exec_delay_time_sec > 0:
+                time.sleep(self.exec_delay_time_sec)
+            t2 = time.time()
+        else:
+            next_ip, next_port = '172.17.49.51', 18800
+            log.info('push data to {}:{}'.format(next_ip, next_port))
+            result = ClientGen.call(next_ip, next_port, 'push', data)
         if self.exec_delay_time_sec > 0:
             time.sleep(self.exec_delay_time_sec)
         t2 = time.time()
+        # logging
+        log.info('compute result: {}'.format(result))
+        
         log.info('(TIME) stop computation: {}'.format(t2))
         log.info('computation result:\n{}'.format(result))
         return 'exec_time: {}, result:\n{}'.format(t2-t1, result)
