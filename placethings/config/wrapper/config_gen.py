bae6782c06e61e3cc563d22b026ae70b94c08ff9
kumokay <kumokay@github.com>
2018-11-06 00:11:56 -0800

    just save file

---
+++ placethings/config/wrapper/config_gen.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from future.utils import iteritems
+
+from placethings.config.wrapper.device_gen import AllDeviceData
+from placethings.config.wrapper.task_gen import AllTaskData
+from placethings.config import spec_def
+
+class Config(object):
+    def __init__(self):
+        self.all_device_data = AllDeviceData()
+        self.all_task_data = AllTaskData()
+        self.DEVICE_SPEC = spec_def.DEVICE_SPEC
+        self.NW_DEVICE_SPEC = spec_def.NW_DEVICE_SPEC
+
+    def add_device(self, device_category, device, num):
+        assert device_category in DEVICE_SPEC
+        assert device in DEVICE_SPEC[device_category]
+        self.all_device_data.add_device(device_category, device, num)
+
+    def add_nw_device(self, nw_device_category, nw_device, num):
+        assert nw_device_category in NW_DEVICE_SPEC
+        assert nw_device in NW_DEVICE_SPEC[nw_device_category]
+        self.all_device_data.add_nw_device(nw_device_category, nw_device, num)
+
+    def add_dev_link(self, dev, nw_dev, latency):
+        self.all_device_data.add_dev_link(dev, nw_dev, latency)
+
+    def add_nw_dev_link(self, src, dst, src_link_type, dst_link_type, latency):
+        self.all_device_data.add_nw_dev_link(
+            src, dst, src_link_type, dst_link_type, latency)
+
+    def add_task(self, task_name):
+        self.all_task_data.add_task(task_name)
+
+    def add_task_flavor(self, task_name, flavor, resrc_rqmt_dict, latency_dict):
+        """
+        args:
+            task_name (str)
+            flavor (Flavor): Flavor.GPU, Flavor.CPU
+            resrc_rqmt_dict: e.g. {
+                Hardware.RAM: Unit.gbyte(1),
+                Hardware.HD: Unit.mbyte(30),
+                Hardware.GPU: Unit.percentage(0),
+                Hardware.CPU: Unit.percentage(60),
+            }
+            latency_dict: e.g. {
+                Device.T2_MICRO: Unit.sec(6),
+                Device.T3_LARGE: Unit.sec(2),
+                Device.P3_2XLARGE: Unit.ms(600),
+            }
+        """
+        self.all_task_data.add_flavor(
+            task_name, flavor, resrc_rqmt_dict, latency_dict)
+
+    def add_task_link(self, src, dst, traffic):
+        self.all_task_data.add_link(src, dst, traffic)
+
+    def add_task_mapping(self, task, device):
+        device_list = self.all_device_data.device_inventory.get_device_list()
+        self.all_task_data.add_mapping(task, device, device_list)
--- placethings/demo/test_config_wrapper.py
+++ placethings/demo/test_config_wrapper.py
@@ -5,20 +5,79 @@ from __future__ import unicode_literals
 
 import logging
 
-from placethings import ilp_solver
-from placethings.config import device_data, nw_device_data
-from placethings.definition import GnInfo, Unit
-from placethings.graph_gen import graph_factory, device_graph
-from placethings.config.config_factory import FileHelper
+from placethings.config.wrapper.config_gen import Config
 from placethings.demo.base_test import BaseTestCase
-
+from placethings.definition import (
+    Device, DeviceCategory, Flavor, Hardware, NwDevice, NwDeviceCategory,
+    GnInfo, GtInfo, LinkType, Unit)
 
 log = logging.getLogger()
 
-_DEFAULT_CONFIG = 'config_default'
+"""
+network settings
+
+CONTROLLER.0   P3_2XLARGE.0     T3_LARGE.0
+       |         |                 |
+CENTER_SWITCH.1 CENTER_SWITCH.0  FIELD_SWITCH.1   (manager)      T2_MICRO.0
+        |       |                 |                 |               |
+      BB_SWITCH.0 --------- BB_SWITCH.1 ---- BB_SWITCH.2 ----FIELD_SWITCH.0
+        |                    |                 |
+     BB_AP.0               BB_AP.1         BB_AP.2
+        |
+     CAMERA.0
+"""
 
 
 class TestConfigWrapper(BaseTestCase):
     @staticmethod
     def test(config_name=None, is_export=False):
         assert config_name is None
+        cfg = Config()
+
+        cfg.add_nw_device(NwDeviceCategory.FIELD, NwDevice.FIELD_SWITCH, 2)
+        cfg.add_nw_device(NwDeviceCategory.BACKBONE, NwDevice.BB_SWITCH, 3)
+        cfg.add_nw_device(NwDeviceCategory.BACKBONE, NwDevice.BB_AP, 3)
+        cfg.add_nw_device(NwDeviceCategory.CENTER, NwDevice.CENTER_SWITCH, 2)
+
+        cfg.add_nw_dev_link(
+            'CENTER_SWITCH.1', 'BB_SWITCH.0',
+            LinkType.WAN, LinkType.ANY, Unit.ms(2))
+        cfg.add_nw_dev_link(
+            'CENTER_SWITCH.0', 'BB_SWITCH.0',
+            LinkType.WAN, LinkType.ANY, Unit.ms(2))
+        cfg.add_nw_dev_link(
+            'FIELD_SWITCH.1', 'BB_SWITCH.1',
+            LinkType.WAN, LinkType.ANY, Unit.ms(2))
+        cfg.add_nw_dev_link(
+            'FIELD_SWITCH.1', 'BB_SWITCH.2',
+            LinkType.WAN, LinkType.ANY, Unit.ms(2))
+        cfg.add_nw_dev_link(
+            'BB_AP.0', 'BB_SWITCH.0',
+            LinkType.WAN, LinkType.ANY, Unit.ms(2))
+        cfg.add_nw_dev_link(
+            'BB_AP.1', 'BB_SWITCH.1',
+            LinkType.WAN, LinkType.ANY, Unit.ms(2))
+        cfg.add_nw_dev_link(
+            'BB_AP.2', 'BB_SWITCH.2',
+            LinkType.WAN, LinkType.ANY, Unit.ms(2))
+        cfg.add_nw_dev_link(
+            'BB_SWITCH.0', 'BB_SWITCH.1',
+            LinkType.ANY, LinkType.ANY, Unit.ms(2))
+        cfg.add_nw_dev_link(
+            'BB_SWITCH.1', 'BB_SWITCH.2',
+            LinkType.ANY, LinkType.ANY, Unit.ms(2))
+
+        cfg.add_device(DeviceCategory.SENSOR, Device.CAMERA, 1)
+        cfg.add_device(DeviceCategory.PROCESSOR, Device.P3_2XLARGE, 1)
+        cfg.add_device(DeviceCategory.PROCESSOR, Device.T3_LARGE, 2)
+        cfg.add_device(DeviceCategory.PROCESSOR, Device.T2_MICRO, 4)
+        cfg.add_device(DeviceCategory.ACTUATOR, Device.CONTROLLER, 1)
+
+        cfg.add_dev_link('CAMERA.0', 'BB_AP.0', Unit.ms(30))
+        cfg.add_dev_link('P3_2XLARGE.0', 'CENTER_SWITCH.0', Unit.ms(2))
+        cfg.add_dev_link('T3_LARGE.0', 'FIELD_SWITCH.1', Unit.ms(2))
+        cfg.add_dev_link('T2_MICRO.0', 'FIELD_SWITCH.0', Unit.ms(2))
+        cfg.add_dev_link('CONTROLLER.0', 'CENTER_SWITCH.1', Unit.ms(2))
+
+        # TODO: fuuuu GtInfoEnum.EXEC_CMD
+        cfg.add_task()
